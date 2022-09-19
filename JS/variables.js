831534d455c4597d95add5121cac662a424aa7c7
Devanada <ydevanada@gmail.com>
2022-09-19 11:06:27 +0700

    Up for day 1 JS

---
+++ JS/variables.js
@@ -0,0 +1,48 @@
+/*
+JavaScript adalah sebuah bahasa pemrograman yang dinamis, artinya sebuah variabel kondisi pertamanya bisa berupa string untuk tipe datanya, akan tetapi dia bisa berubah tipe datanya menjadi number atau yang lainnya.
+*/
+// ; -> semicolon
+// Ini variabel, namanya message nilainya "hello world"
+let message = "hello world";
+message = 123456;
+console.log(message);
+
+// ---------- HOISTING ----------
+console.log(variableVar);
+
+// ---------- REDECLARE ----------
+// X
+// const variableConst = "testing";
+// const variableConst = "halo";
+
+// X
+// let variableLet = "testing";
+// let variableLet = "halo";
+
+// V
+var variableVar = "testing";
+var variableVar = "halo";
+
+// ---------- REASSIGN ----------
+// V
+// let cobaCoba = "testing";
+// cobaCoba = 123;
+// console.log(cobaCoba);
+
+// V
+// var cobaCoba2 = "testing";
+// cobaCoba2 = 123;
+// console.log(cobaCoba2);
+
+// X
+// const cobaCoba3 = "testing";
+// cobaCoba3 = 123;
+// console.log(cobaCoba3);
+
+/*
+camelCase -> variableVarSatu
+PascalCase -> VariableVarSatu
+snake_case -> variable_var_var
+UPPERCASE -> VARIABLEVARSATU
+lowercase -> variablevarsatu
+*/
---
+++ JS/variables.ts
@@ -0,0 +1,9 @@
+/*
+TypeScript adalah sebuah bahasa pemrograman yang statis, yang mana turunan langsung dari JavaScript. Karena dia statis artinya sebuah variabel tidak bisa diganti tipe datanya.
+*/
+
+// Ini variabel, namanya message nilainya "hello world"
+let message = "hello world";
+message = "halo dunia";
+message = 123456;
+console.log(message);
