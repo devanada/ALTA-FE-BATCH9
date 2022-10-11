1c7be8270fb3367b62ef80612483565aca0c7073
Devanada <ydevanada@gmail.com>
2022-10-11 11:17:33 +0700

    Testing auto deploy

--- film-app/src/components/Layout.jsx
+++ film-app/src/components/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = ({ children }) => {
     <div className="w-full h-screen">
       <nav className="w-full p-4 bg-alta-primary sticky top-0 flex flex-col md:flex-row gap-2 items-center justify-between">
         <Link to="/">
-          <h1 className="text-white font-mukta">Rata Alada</h1>
+          <h1 className="text-white font-mukta font-bold">Rata Alada</h1>
         </Link>
         <div className="space-x-4">
           <ButtonPrimary
